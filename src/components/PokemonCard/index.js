7417222bee83c8bff294a5c5ac7068f8196fa0ec
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-22 17:40:56 -0300

    chore: infopokemoncards

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const PokemonCard = () => {
+const PokemonCard = ({ pokemon, isLoading }) => {
+  const [imagePokemon, setImagePokemon] = useState("");
+  const [pokemonIndex, setPokemonIndex] = useState("");
+
+  useEffect(() => {
+    const pokeIndex = pokemon.url.split("/")[pokemon.url.split("/").length - 2];
+
+    const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeIndex}.png`;
+
+    setImagePokemon(imgUrl);
+    setPokemonIndex(pokeIndex);
+  }, [pokemon.url]);
+
   return (
     <div className="col-md-3 col-sm-6 mb-5">
       <div className="card">
-        <div className="card-header">
-          <h1>teste</h1>
+        <h5 className="card-header">{pokemonIndex}</h5>
+        <div className="card-body mx-auto">
+          <h6 className="card-title ">
+            {pokemon.name
+              .toLowerCase()
+              .split(" ")
+              .map(
+                (letter) => letter.charAt(0).toUpperCase() + letter.substring(1)
+              )
+              .join(" ")}
+          </h6>
+
+          <img src={imagePokemon} alt="" />
         </div>
       </div>
     </div>
--- src/components/PokemonList/index.js
+++ src/components/PokemonList/index.js
@@ -1,14 +1,32 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 import PokemonCard from "../PokemonCard";
 
 const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await api.get(`/pokemon`);
+
+      console.log(response.data.results);
+      setPokemons(response.data.results);
+      setIsLoading(false);
+    };
+
+    fetchPokemons();
+  }, []);
+
   return (
     <div className="row">
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
+      {pokemons.map((pokemon) => (
+        <PokemonCard
+          key={pokemon.name}
+          pokemon={pokemon}
+          isLoading={isLoading}
+        />
+      ))}
     </div>
   );
 };
