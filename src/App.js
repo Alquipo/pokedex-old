bf261a4c573e5b5d25dea1230f0692636e5690f5
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-22 15:26:48 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./components/NavBar";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container">
+        <Dashboard />
+      </div>
     </div>
   );
 }
