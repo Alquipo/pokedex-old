63f772659c6fc15a0910dcf0debf3e988d6f7976
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-22 15:26:09 -0300

    feat: PokemonCad component

---
+++ src/components/PokemonCard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PokemonCard = () => {
+  return (
+    <div className="col-md-3 col-sm-6 mb-5">
+      <div className="card">
+        <div className="card-header">
+          <h1>teste</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCard;
