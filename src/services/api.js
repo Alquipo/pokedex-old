883830b088ee77bcd920f4d0463ecefdbaa0389a
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-25 03:43:59 -0300

    feat: api Image Pokemon

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// export const getPokemonImageUrl = (id) =>
-//   `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
-
-// export const getPokemonUrl = (id) => `https://pokeapi.co/api/v2${id}`;
+export const getPokemonImageUrl = (id) =>
+  `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
 
 const api = axios.create({
   baseURL: "https://pokeapi.co/api/v2/",
